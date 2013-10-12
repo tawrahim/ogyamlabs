36c949ab154a28db1521a805282e3ef0c573941f
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-10-12 18:28:46 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Ogyam Labs
+
+This is the homepage for ogyam labs.  
+
+Mohammad Tahawi  
+Tawheed Raheem
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ogyamlabs::Application.config.secret_key_base = 'a2b922697a7cd8046afb2b7e3c248ab3742711069fd60ff6fb0983788fc02f9db132e4b530b234887a9169b606a63c542caf59b796073e823a0080498a46dec7'
